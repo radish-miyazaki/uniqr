56216bd1bc18781b9ed6237202af3e36315a4e47
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-02-12 12:36:46 +0900

    impl run func & refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
+use std::io::Write;
 
 use clap::Parser;
 
@@ -29,15 +30,55 @@ pub fn run(args: Args) -> MyResult<()> {
     let mut file = open(&args.in_file)
         .map_err(|e| format!("{}: {}", args.in_file, e))?;
 
+    let mut out_file: Option<File> = None;
+    if let Some(name) = &args.out_file {
+        out_file = Some(File::create(name)?);
+    }
+
+    count_and_output_duplicate_lines(args, &mut file, &mut out_file)?;
+
+    Ok(())
+}
+
+fn count_and_output_duplicate_lines(
+    args: Args,
+    file: &mut Box<dyn BufRead>,
+    mut out_file: &mut Option<File>,
+) -> MyResult<()> {
     let mut line = String::new();
+    let mut prev_line = String::new();
+    let mut line_count: usize = 0;
+
     loop {
         let bytes = file.read_line(&mut line)?;
         if bytes == 0 {
             break;
         }
-        print!("{}", line);
+
+        if line.trim_end() == prev_line.trim_end() {
+            line_count += 1;
+        } else {
+            output_line_to_destination(
+                args.count,
+                &mut out_file,
+                prev_line.clone(),
+                line_count,
+            );
+
+            line_count = 1;
+            prev_line = line.clone();
+        }
+
         line.clear();
     }
+
+    // INFO: 最終行を出力するために、ループの外で再度呼び出す
+    output_line_to_destination(
+        args.count,
+        &mut out_file,
+        prev_line.clone(),
+        line_count,
+    );
 
     Ok(())
 }
@@ -46,5 +87,47 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+
+fn write_line_with_optional_count(
+    count: bool,
+    file: &mut File,
+    line: String,
+    line_count: usize,
+) {
+    if count {
+        write!(file, "{:>4} {}", line_count, line).unwrap();
+    } else {
+        write!(file, "{}", line).unwrap();
+    }
+}
+
+fn print_line_with_optional_count(
+    count: bool,
+    line: String,
+    line_count: usize,
+) {
+    if count {
+        print!("{:>4} {}", line_count, line);
+    } else {
+        print!("{}", line);
+    }
+}
+
+fn output_line_to_destination(
+    count: bool,
+    out_file: &mut Option<File>,
+    line: String,
+    line_count: usize,
+) {
+    if line_count <= 0 {
+        return;
+    }
+
+    if let Some(ref mut file) = out_file {
+        write_line_with_optional_count(count, file, line, line_count);
+    } else {
+        print_line_with_optional_count(count, line, line_count);
     }
 }
